a3e4895f8bc3b19c6e72e990a5d08d55b01986e7
Himanshu <hgusain088@gmail.com>
2024-02-13 18:24:35 +0530

    setup register page for post request

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,18 @@
 import express from "express"
-
+import cors from "cors"
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(cors())
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body
+  res.json({ name, email, password })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
